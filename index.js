fde21ddc04cd647b6ae199cb62fa9e6d81236e4e
Anindya <anindya.dey@vistaprint.com>
2021-11-25 14:27:47 +0530

    add download function

--- index.js
+++ index.js
@@ -3,10 +3,17 @@
 const { program } = require('commander');
 
 const list = require('./commands/list')
+const download = require('./commands/download')
+const dir = require('./commands/dir')
 
 program
   .command('list')
   .description('List all the downloaded versions of terraform')
   .action(list)
+
+program
+  .command('download <version>')
+  .description('Downloads a specific version of terraform')
+  .action(download)
 
 program.parse()
