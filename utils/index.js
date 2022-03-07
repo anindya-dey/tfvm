bca5c949b6acc871899dd8828c34289746fb632c
Anindya Dey <andy.ani1992@gmail.com>
2022-03-07 17:51:00 +0530

    refactor

---
+++ utils/index.js
@@ -0,0 +1,15 @@
+import { printError, printInfo, printSuccess } from "./print.js";
+import isTerraformLink from "./isTerraformLink.js";
+import extractTerraformLink from "./extractTerraformLink.js";
+import listTerraformExecutables from "./listTerraformExecutables.js";
+import extractTerraformExecutable from "./extractTerraformExecutable.js";
+
+export {
+    printError,
+    printInfo,
+    printSuccess,
+    isTerraformLink,
+    extractTerraformLink,
+    listTerraformExecutables,
+    extractTerraformExecutable,
+}
