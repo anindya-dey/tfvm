0d6e02ebe9be274b355321ad00cda1fa310ab5c1
Anindya Dey <andy.ani1992@gmail.com>
2022-03-22 14:26:19 +0530

    upgrade pkg

--- utils/get-terraform-versions.js
+++ utils/get-terraform-versions.js
@@ -1,36 +1,45 @@
-const cheerio = require('cheerio');
-const got = require('got');
+const cheerio = require("cheerio");
+const got = require("got");
 
-const { TERRAFORM_DOWNLOAD_URL } = require('../config');
-const { printError } = require('../utils/print');
+const { TERRAFORM_DOWNLOAD_URL } = require("../config");
+const { printError } = require("../utils/print");
 
 function isTerraformLink(i, link) {
-    // Return false if there is no href attribute.
-    if (typeof link.attribs.href === 'undefined') { return false }
+  // Return false if there is no href attribute.
+  if (typeof link.attribs.href === "undefined") {
+    return false;
+  }
 
-    return link.attribs.href.startsWith('/terraform/');
+  return link.attribs.href.startsWith("/terraform/");
 }
 
 function getTerraformVersions() {
-    const terraformVersions = [];
+  const terraformVersions = [];
 
-    return got(TERRAFORM_DOWNLOAD_URL)
-    .then(response => {
-        const $ = cheerio.load(response.body)
+  return got(TERRAFORM_DOWNLOAD_URL)
+    .then((response) => {
+      const $ = cheerio.load(response.body);
 
-        $('a').filter(isTerraformLink).each((i, link) => {
-            const href = link.attribs.href.replace(/^\/terraform\//, '').replace(/\/$/, '');
-            terraformVersions.push(href)
+      $("a")
+        .filter(isTerraformLink)
+        .each((i, link) => {
+          const href = link.attribs.href
+            .replace(/^\/terraform\//, "")
+            .replace(/\/$/, "");
+          terraformVersions.push(href);
         });
 
-        return terraformVersions
-    }).catch(err => {
-        if (err.code === 'ENOTFOUND') {
-            printError(`Could not connect to ${TERRAFORM_DOWNLOAD_URL}. Check your internet connection!`);
-        } else {
-            printError('Something is wrong. Please try again later.')
-        }
+      return terraformVersions;
     })
+    .catch((err) => {
+      if (err.code === "ENOTFOUND") {
+        printError(
+          `Could not connect to ${TERRAFORM_DOWNLOAD_URL}. Check your internet connection!`
+        );
+      } else {
+        printError("Something is wrong. Please try again later.");
+      }
+    });
 }
 
 module.exports = getTerraformVersions;
